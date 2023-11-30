33f82b3c9af437be2acdc6b0fdcbf34cd528e5a9
Krunal Jumde <KrunalJumde24@gmail.com>
2023-11-30 08:32:38 +0530

    Added changes in UI for update/delete functionality

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import deleteIcon from './delete-icon-100.png'
+import editIcon from './icons8-edit-100.png'
 
+import './users.css'
 
 
 let User = () => {
-    let fetchCustData = () => {
-        axios.get('http://localhost:8080/users')
-            .then((response) => {
-                // console.log(response);
-            }).catch((error) => {
-                console.log(error);
-            }).finally(() => {
-                // console.log('API Executed.');
-            });
-    }
-
     const [userData, setUserData] = useState([])
     useEffect(() => {
         axios.get('http://localhost:8080/users')
             .then((response) => {
-                // console.log(response.data);
                 setUserData(response.data)
             }).catch((error) => {
                 throw error;
-            }).finally(() => {
-                // console.log('API Executed.');
             })
     }, [])
 
+
+
+    const [dataToUpdt, setDataToUpdt] = useState({
+        id: '',
+        name: '',
+        email: '',
+        mobile: '',
+        location: ''
+    })
+    let handleSomething = (data) => {
+        setDataToUpdt(data)
+    }
+
+    let handleDataChange = (event) => {
+        const [name, value] = event.target;
+        // setDataToUpdt({ ...dataToUpdt, name: value })
+
+    }
+
+    let deleteUserById = (id) => {
+        axios.delete('http://localhost:8080/deleteUserById', {
+            params: {
+                userId : id
+            }
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error
+            );
+        })
+    }
 
     return (
         <>
@@ -40,6 +60,7 @@ let User = () => {
                         <th scope="col">Email</th>
                         <th scope="col">Mobile</th>
                         <th scope="col">Location</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -52,12 +73,85 @@ let User = () => {
                                 <td>{data.email}</td>
                                 <td>{data.mobile}</td>
                                 <td>{data.location}</td>
+                                <td>
+                                    <button
+                                        className="btn btn-sm btn-primary mx-1"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#userDetailEditModal"
+                                        onClick={() => { handleSomething(data) }}
+                                    >
+                                        Edit
+                                    </button>
+                                    {/* <button type="button" className="btn btn-sm">
+                                        <img className="actionIcons" src={editIcon} alt="" srcset="" />
+                                    </button>
+                                    <button type="button" className="btn btn-sm">
+                                        <img className="actionIcons" src={deleteIcon} alt="" srcset="" />
+                                    </button> */}
+                                    <button
+                                        type="button"
+                                        className="btn btn-sm btn-danger mx-1"
+                                        onClick={() => { deleteUserById(data.id) }}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                         )
                     })}
                 </tbody>
-            </table>
-            <button className="btn btn-sm btn-success" onClick={fetchCustData}>Fetch User Data</button>
+            </table >
+            <div
+                className="modal fade"
+                id="userDetailEditModal"
+                tabIndex="-1"
+                aria-labelledby="userDetailEditModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5>Edit User Details</h5>
+                            <button
+                                type="button" className="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close">
+                            </button>
+
+                        </div>
+                        <div className="modal-body">
+
+                            <form>
+                                <div>
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        className="form-control form-control-sm"
+                                        value={dataToUpdt.name}
+                                        onChange={(event) => { handleDataChange(event) }}
+                                    />
+                                </div>
+                            </form>
+
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button" className="btn btn-sm btn-warning" data-bs-dismiss="modal"
+                                aria-label="Close">
+                                Close
+                            </button>
+                            <button
+                                className="btn btn-sm btn-primary">
+                                Save Changes
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
         </>
     )
 }
